03f12e02b389370e8955ea80d642d80bff7056cb
Daniel Klein <daniel.klein@videotron.ca>
2020-12-21 09:26:21 -0500

    Update Product.php

--- Product.php
+++ Product.php
@@ -8,13 +8,8 @@ class Product
     private float $price;
     private int $availableQuantity;
 
-    /**
-     * Product constructor.
-     *
-     * @param int    $id
-     * @param string $title
-     * @param float  $price
-     * @param int    $availableQuantity
+    /*
+     * Product constructor
      */
     public function __construct($id, $title, $price, $availableQuantity)
     {
@@ -24,91 +19,59 @@ class Product
         $this->availableQuantity = $availableQuantity;
     }
 
-    /**
-     * @return int
-     */
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     */
     public function setId($id)
     {
         $this->id = $id;
     }
 
-    /**
-     * @return string
-     */
     public function getTitle()
     {
         return $this->title;
     }
 
-    /**
-     * @param string $title
-     */
     public function setTitle($title)
     {
         $this->title = $title;
     }
 
-    /**
-     * @return float
-     */
     public function getPrice()
     {
         return $this->price;
     }
 
-    /**
-     * @param float $price
-     */
     public function setPrice($price)
     {
         $this->price = $price;
     }
 
-    /**
-     * @return int
-     */
     public function getAvailableQuantity()
     {
         return $this->availableQuantity;
     }
 
-    /**
-     * @param int $availableQuantity
-     */
     public function setAvailableQuantity($availableQuantity)
     {
         $this->availableQuantity = $availableQuantity;
     }
 
-    /**
+    /*
      * Add Product $product into cart. If product already exists inside cart
      * it must update quantity.
      * This must create CartItem and return CartItem from method
-     * Bonus: $quantity must not become more than whatever
-     * is $availableQuantity of the Product
-     *
-     * @param Cart $cart
-     * @param int  $quantity
-     * @return \CartItem
-     * @throws \Exception
      */
+    
     public function addToCart(Cart $cart, int $quantity)
     {
         return $cart->addProduct($this, $quantity);
     }
 
-    /**
+    /*
      * Remove product from cart
-     *
-     * @param Cart $cart
      */
     public function removeFromCart(Cart $cart)
     {

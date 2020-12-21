06e76010cdec26f02ef7b2c4002d1570c6346984
Daniel Klein <daniel.klein@videotron.ca>
2020-12-21 09:23:13 -0500

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -3,38 +3,22 @@
 
 class Cart
 {
-    /**
-     * @var CartItem[]
-     */
     private array $items = [];
 
-    /**
-     * @return \CartItem[]
-     */
     public function getItems()
     {
         return $this->items;
     }
 
-    /**
-     * @param \CartItem[] $items
-     */
     public function setItems($items)
     {
         $this->items = $items;
     }
 
-    /**
+    /*
      * Add Product $product into cart. If product already exists inside cart
      * it must update quantity.
      * This must create CartItem and return CartItem from method
-     * Bonus: $quantity must not become more than whatever
-     * is $availableQuantity of the Product
-     *
-     * @param Product $product
-     * @param int     $quantity
-     * @return \CartItem
-     * @throws \Exception
      */
     public function addProduct(Product $product, int $quantity)
     {
@@ -53,20 +37,16 @@ class Cart
         return $this->items[$productId] ?? null;
     }
 
-    /**
+    /*
      * Remove product from cart
-     *
-     * @param Product $product
      */
     public function removeProduct(Product $product)
     {
         unset($this->items[$product->getId()]);
     }
 
-    /**
+    /*
      * This returns total number of products added in cart
-     *
-     * @return int
      */
     public function getTotalQuantity()
     {
@@ -77,10 +57,8 @@ class Cart
         return $sum;
     }
 
-    /**
+    /*
      * This returns total price of products added in cart
-     *
-     * @return float
      */
     public function getTotalSum()
     {
